27477b1498c4e8b034e24c233512f180b1c6d799
hobbang <jys6367>
2020-04-25 13:24:51 +0900

    join password encoding

--- src/test/java/com/hobbang/account/AccountControllerTest.java
+++ src/test/java/com/hobbang/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.hobbang.account;
 
+import com.hobbang.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
@@ -56,15 +57,28 @@ class AccountControllerTest {
     @DisplayName("회원 가입 처리 - 입력값 정상")
     @Test
     public void signUpSubmit_with_correct_input() throws Exception {
+        String inputPassword = "1234567890";
+        String inputEmail = "hobbang@naver.com";
+
         mockMvc.perform(post("/sign-up")
                     .param("nickname", "hobbang")
-                    .param("email", "hobbang@naver.com")
-                    .param("password", "1234567890")
+                    .param("email", inputEmail)
+                    .param("password", inputPassword)
                     .with(csrf()))
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
 
-        assertTrue(accountRepository.existsByEmail("hobbang@naver.com"));
+        Account account = accountRepository.findByEmail(inputEmail);
+
+        assertNotNull(account);
+
+        // 비밀번호가 암호화 되는지
+        assertNotEquals(account.getPassword(), inputPassword);
+
+        // db에 저장됬나
+        assertTrue(accountRepository.existsByEmail(inputEmail));
+
+        // 메일 발송됬나
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
