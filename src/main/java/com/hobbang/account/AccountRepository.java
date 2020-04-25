113a1bab9be3f13e78f2acfc891629ca5479a704
hobbang <jys6367>
2020-04-25 11:47:19 +0900

    check join form value

---
+++ src/main/java/com/hobbang/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.hobbang.account;
+
+import com.hobbang.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+}
